76588290f0124d0fcca82ccb4ae9b3f803723f56
Tatiane Chiarello <tatichiarello@gmail.com>
2022-02-16 18:47:09 +0100

    feat: add update and delete func frontend

--- frontend/src/utils/utils.js
+++ frontend/src/utils/utils.js
@@ -1,9 +1,40 @@
-export async function fetchItems() {
+export async function getItems() {
+  const response = await fetch('http://localhost:3001/tasks');
+  const json = await response.json();
+  return json;
+}
+
+export async function postItems(task) {
   const response = await fetch('http://localhost:3001/tasks', {
-    method: 'GET',
+    method: 'POST',
     headers: {
       'Content-Type': 'application/json',
     },
+    body: JSON.stringify(task),
+  });
+  const json = await response.json();
+  return json;
+}
+
+export async function updateItem(task) {
+  const response = await fetch('http://localhost:3001/tasks', {
+    method: 'PUT',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(task),
+  });
+  const json = await response.json();
+  return json;
+}
+
+export async function deleteItem(task) {
+  const response = await fetch('http://localhost:3001/tasks', {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(task),
   });
   const json = await response.json();
   return json;
