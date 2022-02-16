d683dcc23b776aff14ed85f4e3cb3d972724a8e7
Tatiane Chiarello <tatichiarello@gmail.com>
2022-02-16 22:43:46 +0100

    style: header

--- frontend/src/components/header/Header.js
+++ frontend/src/components/header/Header.js
@@ -1,7 +1,8 @@
 import React from 'react';
+import './Header.css';
 
 const Header = () => {
-  return <div>Todo List</div>;
+  return <h1 className="header">todos</h1>;
 };
 
 export default Header;
