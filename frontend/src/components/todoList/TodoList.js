2bab2d014417c5f8c3fd99d6432129fea6f79291
Tatiane Chiarello <tatichiarello@gmail.com>
2022-02-16 18:48:07 +0100

    feat: add list layout, create task func

--- frontend/src/components/todoList/TodoList.js
+++ frontend/src/components/todoList/TodoList.js
@@ -1,29 +1,64 @@
 import React, { useEffect, useState } from 'react';
-import { fetchItems } from '../../utils/utils';
+import { getItems, postItems } from '../../utils/utils';
 import Header from '../header/Header';
 import Task from '../task/Task';
 
 const TodoList = () => {
-  const [tasks, setTasks] = useState([]);
+  const [tasks, setTasks] = useState(null);
+  const [value, setValue] = useState('');
 
   useEffect(() => {
     const getTasks = async () => {
-      const res = await fetchItems();
+      const res = await getItems();
       setTasks(res);
     };
 
     getTasks();
   }, []);
 
-  console.log(tasks);
+  const handleOnChange = (event) => {
+    setValue(event.target.value);
+  };
+
+  const onSubmit = async (event) => {
+    event.preventDefault();
+    if (!value) return;
+
+    const params = { task: value };
+
+    try {
+      await postItems(params);
+
+      setTasks((curr) => ({ ...curr, allTasks: [...curr.allTasks, params] }));
+    } catch (err) {
+      console.error(err);
+    }
+  };
 
   return (
     <div>
       <Header />
       <div>
-        <Task />
-        <Task />
-        <Task />
+        <form onClick={onSubmit}>
+          <input
+            type="text"
+            value={value}
+            onChange={handleOnChange}
+            placeholder="What needs to be done?"
+            required
+          />
+          <button type="submit" disabled={!value}>
+            Add
+          </button>
+        </form>
+
+        <div>
+          {!tasks && <p>Empty list</p>}
+          {tasks &&
+            tasks.allTasks.map((task, index) => (
+              <Task key={index} item={task} />
+            ))}
+        </div>
       </div>
     </div>
   );
