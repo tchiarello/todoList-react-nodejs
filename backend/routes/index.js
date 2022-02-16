f9e019b6918e045cdbec74b671fe44c9de6e4098
Tatiane Chiarello <tatichiarello@gmail.com>
2022-02-16 18:45:22 +0100

    add delete and update routes backend

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -4,13 +4,13 @@ const router = express.Router();
 const {
   createTask,
   getAllTasks,
-  // updateTaskById,
-  // deleteTaskById,
+  updateTaskById,
+  deleteTaskById,
 } = require('../controllers/tasksController');
 
 router.get('/tasks', getAllTasks);
 router.post('/tasks', createTask);
-// router.put('/:id', updateTaskById);
-// router.delete('/:id', deleteTaskById);
+router.put('/tasks/:id', updateTaskById);
+router.delete('/tasks/:id', deleteTaskById);
 
 module.exports = router;
