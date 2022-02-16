542a749849e8d6d1e79dc8aa15c8225d1511ed96
Tatiane Chiarello <tatichiarello@gmail.com>
2022-02-16 22:47:01 +0100

    feat: add textarea to update task

--- frontend/src/components/task/Task.js
+++ frontend/src/components/task/Task.js
@@ -1,7 +1,25 @@
 import React from 'react';
+import './Task.css';
 
-const Task = () => {
-  return <div>task</div>;
+const Task = ({ id, task, updateTask }) => {
+  const disableEnter = (event) => {
+    if (event.keyCode === 13) {
+      event.preventDefault();
+    }
+    // https://stackoverflow.com/questions/41359780/disable-enter-in-input-box
+  };
+
+  return (
+    <div className="task-content">
+      <textarea
+        className="task-area"
+        onBlur={(event) => updateTask(id, event.target.value)}
+        onKeyDown={disableEnter}
+      >
+        {task}
+      </textarea>
+    </div>
+  );
 };
 
 export default Task;
