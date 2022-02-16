5983e78485244d3ae33a1f6f4daaa25502e68b55
Tatiane Chiarello <tatichiarello@gmail.com>
2022-02-16 17:18:41 +0100

    feat: create controller, createTask and getAllTasks func

---
+++ backend/controllers/tasksController.js
@@ -0,0 +1,63 @@
+const TasksService = require('../services/tasksService');
+
+const createTask = async (req, res, next) => {
+  try {
+    const { task } = req.body;
+
+    const newTask = await TasksService.createTask({ task });
+
+    return res.status(201).json(newTask);
+  } catch (error) {
+    console.log(error.message);
+    return next(error);
+  }
+};
+
+const getAllTasks = async (_req, res, next) => {
+  try {
+    const allTasks = await TasksService.getAllTasks();
+
+    return res.status(200).json({
+      allTasks,
+    });
+  } catch (error) {
+    console.log(error.message);
+    return next(error);
+  }
+};
+
+// const updateTaskById = async (req, res, next) => {
+//   try {
+//     const { id } = req.params;
+//     const { task } = req.body;
+
+//     const taskItem = await TasksService.updateTaskById(id, {
+//       task,
+//     });
+
+//     return res.status(200).json(taskItem);
+//   } catch (error) {
+//     console.log(error.message);
+//     return next(error);
+//   }
+// };
+
+// const deleteTaskById = async (req, res, next) => {
+//   try {
+//     const { id } = req.params;
+
+//     const taskItem = await TasksService.deleteTaskById(id);
+
+//     return res.status(200).json(taskItem);
+//   } catch (error) {
+//     console.log(error.message);
+//     return next(error);
+//   }
+// };
+
+module.exports = {
+  createTask,
+  getAllTasks,
+  // updateTaskById,
+  // deleteTaskById,
+};
